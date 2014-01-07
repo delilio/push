6f09e45c5a8037611514b0c1f8816c2534c01bf0
stark <agbemabiese.dela@gmail.com>
2014-01-07 01:32:29 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Datamines::Application.config.secret_key_base = '81fbfeb4e834578708d5666c512bc275c6c43d542714657a6e0c3da82a08d8eda46b6c8a3b65d1361f114cbd03bd0b732f9acf31d8a43b5c08bb385cedb6be6d'
