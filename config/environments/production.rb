ef49a9cba223682038e4bb241bbb76a7cda1bf15
stark <agbemabiese.dela@gmail.com>
2014-01-19 19:10:54 -0600

    Implemented devise for users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Datamines::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  ##Set to actual hostname
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
 end
